ede7bf9ac87deb9d8576dfe30160dfe953753264
shihab hasan <shihabhasanhridoy06@gmail.com>
2026-04-17 22:56:30 -0700

    webview added

--- app/src/main/java/com/example/homesweethome/activities/LoginActivity.java
+++ app/src/main/java/com/example/homesweethome/activities/LoginActivity.java
@@ -309,10 +309,11 @@ public class LoginActivity extends AppCompatActivity {
                         
                         // Save landlord info to SessionManager
 
+                        sessionManager.saveLandlordInfo(landlord);
+
                         // Navigate to subscription screen
                         if (landlord.getSubscriptionPurchase() != null && landlord.getSubscriptionPurchase().getSubscriptionId() != null
                         && landlord.getSubscriptionPurchase().isPaymentComplete()){
-                            sessionManager.saveLandlordInfo(landlord);
 
                             Intent intent = new Intent(LoginActivity.this, LandlordDashboardActivity.class);
                             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
