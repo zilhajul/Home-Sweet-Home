3d376e6b0c256ddb65836c0fd3d14c31ea863c43
Shihab Hasan <shihab.hasan@v2.ltd>
2026-04-26 20:57:18 +0600

    simple import

--- app/src/main/java/com/example/homesweethome/activities/adapter/FlatAdapter.java
+++ app/src/main/java/com/example/homesweethome/activities/adapter/FlatAdapter.java
@@ -12,6 +12,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.example.homesweethome.R;
+import com.example.homesweethome.model.Flat;
 
 import java.util.List;
 
